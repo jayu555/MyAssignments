475040b293f5d28ea4800b473adf8a8845da1b47
jayu555 <68842984+jayu555@users.noreply.github.com>
2024-10-15 19:30:50 +0530

    Add files via upload
    Day1 Assignment

---
+++ Basics/ChromeDetails.java
@@ -0,0 +1,24 @@
+package Basics;
+
+public class ChromeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float version = 91.0f;
+		String developer = "Google";
+		boolean value = false;
+		int a = 2000;
+		char sh = 'C';
+		
+		System.out.println("Chrome version is : "+version);
+		System.out.println("Devoloped By : "+developer);
+		System.out.println("Beta Type : "+value);
+		System.out.println("Beta Type : "+value);
+		System.out.println("Released Year : "+a);
+		System.out.println("Shortcutkey : "+sh);
+		
+
+	}
+
+}
