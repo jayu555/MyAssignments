b64f8d5fc1315f300a5451af4b3b111ebea7407c
jayu555 <68842984+jayu555@users.noreply.github.com>
2024-10-15 20:22:53 +0530

    Add files via upload
    Day2 Assignments

---
+++ FindEvenNumbers.java
@@ -0,0 +1,26 @@
+package Basics;
+
+public class FindEvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		for(int i=1; i<=10; i++) {
+			
+			if(i%2 == 0) {
+				
+				System.out.println(i);
+			}
+		}
+		
+		
+		//Another Method
+		//for(int i=0; i<=10; i=i+2) {
+			
+			//System.out.println(i);
+		//}
+
+	}
+
+}
---
+++ IsPrime.java
@@ -0,0 +1,28 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class IsPrime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the value of n : ");
+		int n = scan.nextInt();
+		
+		for(int i=2; i<n; i++) {
+			
+			if(n%i == 0) {
+				System.out.println("The Given number is Prime");
+				break;
+			}
+			else {
+				System.out.println("The given number is not Prime");
+				break;
+			}
+			
+		}
+	}
+
+}
